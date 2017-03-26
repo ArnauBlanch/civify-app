ffec75e9dcde542cf6b425f0df367da3e7f5b5b7
sergio.sanchis <sergio.sanchis@est.fib.upc.edu>
2017-03-26 23:05:13 +0200

    task #115: first schedule

---
+++ app/src/main/java/com/civify/civify/activity/EventDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.civify.civify.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.civify.civify.R;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
+}
