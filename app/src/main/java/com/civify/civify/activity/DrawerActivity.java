3d69c581183e707d28a4360bb76231dc78bad97a
dsegoviat <david.segovia@est.fib.upc.edu>
2017-03-29 16:03:09 +0200

    Now you can hide toolbar with wall and profile

--- app/src/main/java/com/civify/civify/activity/DrawerActivity.java
+++ app/src/main/java/com/civify/civify/activity/DrawerActivity.java
@@ -1,6 +1,8 @@
 package com.civify.civify.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -8,28 +10,29 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import com.civify.civify.R;
-import com.civify.civify.activity.model.UserFake;
 
 public class DrawerActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    
+
     private DrawerLayout mDrawerLayout;
-    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
+    private AppBarLayout mAppBarLayout;
     private int mCurrentFragment;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.bar_layout);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,20 +42,20 @@ public class DrawerActivity extends BaseActivity
                         .show();
             }
         });
-        
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-        
-        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
-        mNavigationView.setNavigationItemSelectedListener(this);
-        
-        mNavigationView.getMenu().getItem(0).setChecked(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.getMenu().getItem(0).setChecked(true);
         mCurrentFragment = 0;
     }
-    
+
     @Override
     public void onBackPressed() {
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -61,41 +64,29 @@ public class DrawerActivity extends BaseActivity
             super.onBackPressed();
         }
     }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer, menu);
-        return true;
+
+    private void hideViews() {
+        mAppBarLayout.animate().translationY(-mToolbar.getHeight()).setInterpolator(
+                new AccelerateInterpolator(0));
     }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        
-        return super.onOptionsItemSelected(item);
+
+    private void showViews() {
+        mAppBarLayout.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
     }
-    
-    @SuppressWarnings("StatementWithEmptyBody")
+
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        
+
         if (id == R.id.nav_navigate) {
             mCurrentFragment = 0;
         } else if (id == R.id.nav_wall) {
             mCurrentFragment = 1;
+            hideViews();
         } else if (id == R.id.nav_profile) {
             mCurrentFragment = 2;
+            showViews();
         } else if (id == R.id.nav_rewards) {
             mCurrentFragment = 3;
         } else if (id == R.id.nav_achievements) {
@@ -105,7 +96,7 @@ public class DrawerActivity extends BaseActivity
         } else if (id == R.id.nav_settings) {
             mCurrentFragment = 6;
         }
-        
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
