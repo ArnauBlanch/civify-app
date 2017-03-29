0ad214bc6e0491a050ca07fe9c14301baf6cd7f6
Ricard-PC\Ricard <ricard.fos@est.fib.upc.edu>
2017-03-29 15:32:19 +0200

    Progress

--- app/src/main/java/com/civify/civify/view/LoginActivity.java
+++ app/src/main/java/com/civify/civify/view/LoginActivity.java
@@ -3,15 +3,31 @@ package com.civify.civify.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.civify.civify.R;
 
 
 
 public class LoginActivity extends AppCompatActivity {
+    Button blogin;
+    EditText usr;
+    EditText pasw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.e("error", "Login error");
+        blogin.findViewById(R.id.bsignin);
+        blogin.setOnClickListener(lis);
+        usr.findViewById(R.id.login_email_input);
+        pasw.findViewById(R.id.login_password_input);
     }
+    View.OnClickListener lis = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Log.e("error", "Login error");
+        }
+    };
 }
