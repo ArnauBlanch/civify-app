0c6f907f5c56e07e4fddb00576d37f7d2e075d3e
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-03-29 17:31:52 +0200

    Tasca #98 - Header "Authorization" afegit

--- app/src/main/java/com/civify/civify/service/UserService.java
+++ app/src/main/java/com/civify/civify/service/UserService.java
@@ -6,16 +6,23 @@ import com.google.gson.JsonObject;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface UserService {
+    String AUTHORIZATION =
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHBpcmF0"
+            + "aW9uIjoxNDkwNDQ1MzgyfQ.H6ZNmOQP4AZJFJeSn5Noscv7FVGUoINaND3YkCcrKRc";
 
+    @Headers("Authorization: " + AUTHORIZATION)
     @POST("users")
     Call<MessageResponse> registerUser(@Body User user);
 
+    @Headers("Authorization: " + AUTHORIZATION)
     @POST("users/search")
     Call<MessageResponse> checkUnusedUsername(@Body JsonObject username);
 
+    @Headers("Authorization: " + AUTHORIZATION)
     @POST("users/search")
     Call<MessageResponse> checkUnusedEmail(@Body JsonObject email);
 }
