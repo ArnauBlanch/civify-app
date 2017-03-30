2494ecb6b1290f3fbf9bf457c6970aef30ab5608
dsegoviat <david.segovia@est.fib.upc.edu>
2017-03-30 18:56:17 +0200

    Fixed checkstyle

--- app/src/main/java/com/civify/civify/activity/fragments/AchievementsFragment.java
+++ app/src/main/java/com/civify/civify/activity/fragments/AchievementsFragment.java
@@ -14,44 +14,26 @@ import com.civify.civify.R;
  * create an instance of this fragment.
  */
 public class AchievementsFragment extends Fragment {
-    
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    
+
     public AchievementsFragment() {
         // Required empty public constructor
     }
-    
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     * @return A new instance of fragment NavigateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static AchievementsFragment newInstance() {
         AchievementsFragment fragment = new AchievementsFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
     }
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-    
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_achievements, container, false);
     }
