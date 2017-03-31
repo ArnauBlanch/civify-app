3332da1292159177e70f9c1b40fa1396b3ed3871
Ricard-PC\Ricard <ricard.fos@est.fib.upc.edu>
2017-03-31 02:03:03 +0200

    merge develop + feature 17

--- app/src/main/java/com/civify/civify/view/LoginActivity.java
+++ app/src/main/java/com/civify/civify/view/LoginActivity.java
@@ -7,13 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.civify.civify.R;
-import com.civify.civify.controller.LoginAdapter;
 import com.civify.civify.controller.LoginAdapterImpl;
 import com.civify.civify.controller.LoginError;
 import com.civify.civify.controller.LoginFinishedCallback;
@@ -21,51 +18,56 @@ import com.civify.civify.model.User;
 
 
 public class LoginActivity extends AppCompatActivity {
-    AppCompatButton blogin;
-    EditText usr;
-    EditText pasw;
-    LoginAdapterImpl loginadapterimpl;
-    TextView passforgot;
+    private AppCompatButton mBlogin;
+    private EditText mUser;
+    private EditText mPassw;
+    private LoginAdapterImpl mLoginadapterimpl;
+    private TextView mPassforgot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        blogin = (AppCompatButton) findViewById(R.id.bsignin);
-        blogin.setOnClickListener(lis);
-        usr= (EditText) findViewById(R.id.login_email_input);
-        pasw = (EditText) findViewById(R.id.login_password_input);
-        passforgot = (TextView) findViewById(R.id.login_forgot);
-        passforgot.setOnClickListener(lis);
+        mBlogin = (AppCompatButton) findViewById(R.id.bsignin);
+        mBlogin.setOnClickListener(mListen);
+        mUser = (EditText) findViewById(R.id.login_email_input);
+        mPassw = (EditText) findViewById(R.id.login_password_input);
+        mPassforgot = (TextView) findViewById(R.id.login_forgot);
+        mPassforgot.setOnClickListener(mListen);
         SharedPreferences userpreferences = getSharedPreferences("USERPREFS", Context.MODE_PRIVATE);
-        loginadapterimpl = new LoginAdapterImpl(userpreferences);
+        mLoginadapterimpl = new LoginAdapterImpl(userpreferences);
     }
-    View.OnClickListener lis = new View.OnClickListener(){
+    private View.OnClickListener mListen = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            String tag = "loginLog";
             switch (v.getId()) {
                 case R.id.bsignin:
-                String user = usr.getText().toString();
-                String password = pasw.getText().toString();
-                loginadapterimpl.login(user, password, new LoginFinishedCallback() {
-                    @Override
-                    public void onLoginSucceeded(User u) {
-                        Log.d("login", "login succeed");
-                        //anar a la pantalla de mapa
-                    }
+                    String password = mPassw.getText().toString();
+                    String user = mUser.getText().toString();
+                    mLoginadapterimpl.login(user, password, new LoginFinishedCallback() {
+                        @Override
+                        public void onLoginSucceeded(User u) {
+                            Log.d(tag, "login succeed");
+                            //anar a la pantalla de mapa
+                        }
 
-                    @Override
-                    public void onLoginFailed(LoginError e) {
-                        Log.d("login", e.getType().toString());
-                        Toast toast = Toast.makeText(getApplicationContext(), e.getType().toString(), Toast.LENGTH_SHORT);
-                        toast.show();
-                        //Mostrar l'error per pantalla corresponent
-                    }
-                });
+                        @Override
+                        public void onLoginFailed(LoginError e) {
+                            Log.d(tag, e.getType().toString());
+                            Toast toast = Toast.makeText(getApplicationContext(),
+                                    e.getType().toString(), Toast.LENGTH_SHORT);
+                            toast.show();
+                            //Mostrar l'error per pantalla corresponent
+                        }
+                    });
                     break;
                 case R.id.login_forgot:
-                    Log.d("login", "change pass");
-                    Toast toast = Toast.makeText(getApplicationContext(), "Aquest botó portará a la activity de recuperar password", Toast.LENGTH_SHORT);
+                    Log.d(tag, "change pass");
+                    Toast toast = Toast.makeText(getApplicationContext(), "portará a la activity",
+                            Toast.LENGTH_SHORT);
                     toast.show();
+                    break;
+                default:
                     break;
             }
         }
