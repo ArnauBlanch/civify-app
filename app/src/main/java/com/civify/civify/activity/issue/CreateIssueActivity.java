d9e44b811a41fd39d79d965622091863c6b7c8f1
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-04-08 03:19:55 +0200

    Create issue structure & validations finished (expect category)

--- app/src/main/java/com/civify/civify/activity/issue/CreateIssueActivity.java
+++ app/src/main/java/com/civify/civify/activity/issue/CreateIssueActivity.java
@@ -4,15 +4,23 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.civify.civify.R;
 import com.civify.civify.adapter.IssueAdapter;
+import com.civify.civify.model.Category;
+import com.civify.civify.model.Issue;
+import com.civify.civify.model.Picture;
+import com.civify.civify.service.IssueSimpleCallback;
 import com.civify.civify.utils.AdapterFactory;
 
 public class CreateIssueActivity extends CameraGalleryActivity {
@@ -75,17 +83,73 @@ public class CreateIssueActivity extends CameraGalleryActivity {
 
     public void titleButtonListener(View v) {
         EditText title = (EditText) findViewById(R.id.title_input);
+        TextInputLayout titleLayout = (TextInputLayout) findViewById(R.id.title_input_layout);
         if (title.getText().length() == 0) {
-            title.setError(getString(R.string.must_insert_issue_title));
+            titleLayout.setError(getString(R.string.must_insert_issue_title));
         } else {
             title.setError(null);
             nextPage();
         }
     }
 
+    public void categoryButtonListener(View v) {
+        nextPage();
+    }
+
+    public void photoButtonListener(View v) {
+        ImageView imageView = (ImageView) findViewById(R.id.photo_view);
+        TextView message = (TextView) findViewById(R.id.photo_validation);
+        if (imageView.getDrawable() != null) {
+            nextPage();
+        } else {
+            message.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void riskButtonListener(View v) {
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        TextView message = (TextView) findViewById(R.id.risk_validation);
+        if (radioGroup.getCheckedRadioButtonId() != -1) {
+            message.setVisibility(View.INVISIBLE);
+            nextPage();
+        } else {
+
+            message.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void descriptionButtonListener(View v) {
+        String title = ((TextView) findViewById(R.id.title_input)).getText().toString();
+        String description = ((TextView) findViewById(R.id.description_input)).getText().toString();
+        Category category = null;
+        boolean risk = ((RadioGroup) findViewById(R.id.radio_group)).getCheckedRadioButtonId() == 0;
+        float longitude = 0.0f;
+        float latitude = 0.0f;
+        Picture picture = null;
+        String userAuthToken = null;
+
+        Issue newIssue = new Issue(title, description, category, risk,
+                longitude, latitude, picture, userAuthToken);
+        mIssueAdapter.createIssue(newIssue, new IssueSimpleCallback() {
+            @Override
+            public void onSuccess(Issue issue) {
+                // Issue successfully created
+            }
+
+            @Override
+            public void onFailure() {
+                Snackbar.make(findViewById(R.id.create_issue_linearlayout),
+                        getString(R.string.couldnt_create_issue), Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+
     @Override
     protected void handlePhotoResult(Bitmap imageBitmap) {
         ImageView imageView = (ImageView) findViewById(R.id.photo_view);
         imageView.setImageBitmap(imageBitmap);
+        TextView message = (TextView) findViewById(R.id.photo_validation);
+        message.setVisibility(View.GONE);
     }
 }
