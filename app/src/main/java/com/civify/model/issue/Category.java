4394e6db7e75d4798ac7da432b755276e8c99089
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-04-09 22:46:40 +0200

    Remove marker selected from category

--- app/src/main/java/com/civify/model/issue/Category.java
+++ app/src/main/java/com/civify/model/issue/Category.java
@@ -8,59 +8,49 @@ import com.google.gson.annotations.SerializedName;
 public enum Category {
     // Senyalitació vial
     @SerializedName("road_signs")
-    ROAD_SIGNS(R.drawable.civify, R.drawable.civify, R.drawable.civify, R.string
+    ROAD_SIGNS(R.drawable.civify, R.drawable.civify, R.string
             .road_signs),
 
     // Il·lumincació
     @SerializedName("illumination")
-    ILLUMINATION(R.drawable.civify, R.drawable.civify, R.drawable.civify, R.string.illumination),
+    ILLUMINATION(R.drawable.civify, R.drawable.civify, R.string.illumination),
 
     // Arbolada
     @SerializedName("grove")
-    GROVE(R.drawable.civify, R.drawable.civify, R.drawable.civify, R.string.grove),
+    GROVE(R.drawable.civify, R.drawable.civify, R.string.grove),
 
     // Mobiliari urbà
     @SerializedName("street_furniture")
-    STREET_FURNITURE(R.drawable.civify, R.drawable.civify, R.drawable.civify,
-            R.string.street_furniture),
+    STREET_FURNITURE(R.drawable.civify, R.drawable.civify, R.string.street_furniture),
 
     // Escombraries i neteja
     @SerializedName("trash_and_cleaning")
-    TRASH_AND_CLEANING(R.drawable.civify, R.drawable.civify, R.drawable.civify,
-            R.string.trash_and_cleaning),
+    TRASH_AND_CLEANING(R.drawable.civify, R.drawable.civify, R.string.trash_and_cleaning),
 
     // Transport públic
     @SerializedName("public_transport")
-    PUBLIC_TANSPORT(R.drawable.civify, R.drawable.civify, R.drawable.civify,
-            R.string.public_transport),
+    PUBLIC_TANSPORT(R.drawable.civify, R.drawable.civify, R.string.public_transport),
 
     // Suggeriment
     @SerializedName("suggestion")
-    SUGGESTION(R.drawable.civify, R.drawable.civify, R.drawable.civify, R.string.suggestion),
+    SUGGESTION(R.drawable.civify, R.drawable.civify, R.string.suggestion),
 
     // Altres
     @SerializedName("other")
-    OTHER(R.drawable.civify, R.drawable.civify, R.drawable.civify, R.string.other);
+    OTHER(R.drawable.civify, R.drawable.civify, R.string.other);
 
     private final int mMarker;
-    private final int mMarkerSelected;
     private final int mIcon;
     private final int mName;
 
-    Category(@DrawableRes int marker, @DrawableRes int markerSelected, @DrawableRes int icon,
-            int name) {
+    Category(@DrawableRes int marker, @DrawableRes int icon, int name) {
         this.mMarker = marker;
-        this.mMarkerSelected = markerSelected;
         this.mIcon = icon;
         this.mName = name;
     }
 
     public int getMarker() {
         return this.mMarker;
-    }
-
-    public int getMarkerSelected() {
-        return this.mMarkerSelected;
     }
 
     public int getIcon() {
