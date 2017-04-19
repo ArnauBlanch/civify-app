6c7b97731f18db343cb261daee51a88427709738
ricardfos <helena98>
2017-04-19 14:25:37 +0200

    Tasca 120- Layout editIssue no activity

---
+++ app/src/main/java/com/civify/activity/EditIssueActivity.java
@@ -0,0 +1,15 @@
+package com.civify.activity;
+
+import android.os.Bundle;
+
+import com.civify.R;
+
+public class EditIssueActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_issue);
+    }
+
+}
