4146705b4f843e00ae31efb8e8ffe7a70b2cbd43
Sergio Sanchis <sergio.sanchis@est.fib.upc.edu>
2017-04-27 23:06:58 +0200

    Feature-47: Feature acabada provisionalment sense passar checkstyle ni testejada de manera no manual

--- app/src/main/java/com/civify/service/UserService.java
+++ app/src/main/java/com/civify/service/UserService.java
@@ -4,11 +4,20 @@ import com.civify.model.MessageResponse;
 import com.civify.model.User;
 import com.google.gson.JsonObject;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface UserService {
+
+    @GET("/users/{user_auth_token}")
+    Call<User> getUser(@Header("Authorization") String authToken, @Path("user_auth_token")
+            String userAuthToken);
 
     @POST("/users")
     Call<MessageResponse> registerUser(@Body User user);
