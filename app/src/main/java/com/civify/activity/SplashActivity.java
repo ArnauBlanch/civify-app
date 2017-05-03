d8f9338137ed2b0aaa764cd01776fe64dbf580c4
MSI Barcelona <j.espasa.cusachs@accenture.com>
2017-05-03 13:47:00 +0200

    Added splash screen + bugfix on wall (app crashed sometimes)

---
+++ app/src/main/java/com/civify/activity/SplashActivity.java
@@ -0,0 +1,63 @@
+package com.civify.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.civify.R;
+import com.civify.adapter.LoginAdapter;
+import com.civify.adapter.LoginError;
+import com.civify.adapter.LoginFinishedCallback;
+import com.civify.model.User;
+import com.civify.model.map.CivifyMap;
+import com.civify.utils.AdapterFactory;
+import com.google.android.gms.maps.GoogleMap;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends BaseActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                LoginAdapter loginAdapter =
+                        AdapterFactory.getInstance().getLoginAdapter(getApplicationContext());
+                loginAdapter.isLogged(new LoginFinishedCallback() {
+                    @Override
+                    public void onLoginSucceeded(User u) {
+                        Intent intent = new Intent(getApplicationContext(), DrawerActivity.class);
+                        intent.setFlags(
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void onLoginFailed(LoginError t) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.setFlags(
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
