76a61376c83caf8ba5e0f92ca196205acbf223e1
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-06 16:33:28 +0200

    Coins earned dialog after issue creation + API request

--- app/src/main/java/com/civify/activity/fragments/NavigateFragment.java
+++ app/src/main/java/com/civify/activity/fragments/NavigateFragment.java
@@ -72,10 +72,14 @@ public class NavigateFragment extends BasicFragment {
                 } catch (MapNotLoadedException ignore) {
                     // Button to create issues is only enabled if the map is loaded
                 }
+
+                CoinsDialogFragment.addCoins(getActivity(), 1);
+
                 Snackbar.make(getView(), getString(R.string.issue_created),
                         Snackbar.LENGTH_SHORT).show();
             }
             CivifyMap.getInstance().setCanBeDisabled(true);
+
         } else CivifyMap.getInstance().onMapSettingsResults(requestCode, resultCode);
     }
 
