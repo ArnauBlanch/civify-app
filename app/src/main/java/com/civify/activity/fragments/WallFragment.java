3ee9dce9270d9f76da9150b699ea4dca8b6782fd
Ricard-PC\Ricard <ricard.fos@est.fib.upc.edu>
2017-05-17 18:41:09 +0200

    Merge branch 'develop' into feature/120.01

--- app/src/main/java/com/civify/activity/fragments/WallFragment.java
+++ app/src/main/java/com/civify/activity/fragments/WallFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
