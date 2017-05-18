3aabe043dd95a20f16e45b76a2cd41f8623d3cdc
Sergio Sanchis <sergio.sanchis@est.fib.upc.edu>
2017-05-18 23:45:55 +0200

    [FEATURE-176] Init fragment

--- app/src/main/java/com/civify/activity/fragments/reward/ShowQrFragment.java
+++ app/src/main/java/com/civify/activity/fragments/reward/ShowQrFragment.java
@@ -5,10 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.civify.R;
 
 public class ShowQrFragment extends Fragment {
+
+    private View mView;
+
+    private TextView mTitle;
+    private TextView mBusiness;
+    private TextView mDate;
+    private ImageView mQrcode;
+    private TextView mTextQrcode;
+    private TextView mState;
 
     public ShowQrFragment() {
         // Required empty public constructor
@@ -29,7 +40,17 @@ public class ShowQrFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_qr, container, false);
+        mView = inflater.inflate(R.layout.fragment_show_qr, container, false);
+        init();
+        return mView;
+    }
+
+    private void init() {
+        mTitle = (TextView) mView.findViewById(R.id.qr_title);
+        mBusiness = (TextView) mView.findViewById(R.id.qr_business);
+        mDate = (TextView) mView.findViewById(R.id.qr_date);
+        mQrcode = (ImageView) mView.findViewById(R.id.qr_qrcode);
+        mTextQrcode = (TextView) mView.findViewById(R.id.qr_textQrcode);
+        mState = (TextView) mView.findViewById(R.id.qr_state);
     }
 }
