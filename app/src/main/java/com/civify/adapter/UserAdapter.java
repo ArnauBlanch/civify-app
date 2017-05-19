3bb50f2ecf6d463c945d1ddae000ace488971960
Carleslc <lazaro.costa.carles@gmail.com>
2017-05-19 13:52:14 +0200

    Checkstyle fixes

--- app/src/main/java/com/civify/adapter/UserAdapter.java
+++ app/src/main/java/com/civify/adapter/UserAdapter.java
@@ -1,13 +1,11 @@
 package com.civify.adapter;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
-import com.civify.activity.DrawerActivity;
 import com.civify.activity.fragments.RewardDialogFragment;
 import com.civify.model.MessageResponse;
 import com.civify.model.Reward;
