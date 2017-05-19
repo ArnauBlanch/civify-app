56621ef7ea6502941b857285dcf166571871eb6a
Sergio Sanchis <sergio.sanchis@est.fib.upc.edu>
2017-05-19 18:51:54 +0200

    [QR Code] Done

--- app/src/main/java/com/civify/adapter/award/ExchangedAwardAdapter.java
+++ app/src/main/java/com/civify/adapter/award/ExchangedAwardAdapter.java
@@ -5,7 +5,6 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
