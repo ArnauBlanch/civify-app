e7f14dc0e36c2c3c3d353edc344dbe7bfaa89a1c
Sergio Sanchis <sergio.sanchis@est.fib.upc.edu>
2017-06-02 20:35:27 +0200

    [TASK-200] Checkstyle passed

--- app/src/main/java/com/civify/adapter/achievement/AchievementAdapter.java
+++ app/src/main/java/com/civify/adapter/achievement/AchievementAdapter.java
@@ -1,6 +1,5 @@
 package com.civify.adapter.achievement;
 
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
