027582152b82671215396075ea9d7c6e02a39ad5
IvanDeMingo <ivandemingo2309@gmail.com>
2017-06-05 12:38:22 +0200

    Fix achievements list view

--- app/src/main/java/com/civify/activity/fragments/achievements/AchievementViewFragment.java
+++ app/src/main/java/com/civify/activity/fragments/achievements/AchievementViewFragment.java
@@ -1,6 +1,7 @@
 package com.civify.activity.fragments.achievements;
 
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,13 +15,17 @@ import com.civify.activity.fragments.BasicFragment;
 import com.civify.adapter.achievement.AchievementViewAdapter;
 import com.civify.model.achievement.Achievement;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AchievementViewFragment extends BasicFragment {
 
-    private View mView;
+    private List<Achievement> mAchievementList;
+    private AchievementViewAdapter mAchievementAdapter;
 
     public AchievementViewFragment() {
+        mAchievementList = new ArrayList<>();
     }
 
     public static AchievementViewFragment newInstance() {
@@ -43,26 +48,30 @@ public class AchievementViewFragment extends BasicFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+
+        mAchievementAdapter = new AchievementViewAdapter(mAchievementList, getContext());
+
         // Inflate the layout for this fragment
-        mView = inflater.inflate(R.layout.fragment_achievement_view, container, false);
-        return mView;
+        View view = inflater.inflate(R.layout.fragment_achievement_view, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.achievement_recyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(recyclerView.getContext(), LinearLayoutManager.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setAdapter(mAchievementAdapter);
+
+        return view;
     }
 
     public void setAchievementsList(List<Achievement> items) {
-        RecyclerView achievementRecycler = (RecyclerView) mView.findViewById(R.id
-                .achievement_recyclerView);
-        achievementRecycler.setHasFixedSize(true);
+        mAchievementList.clear();
+        ArrayList<Achievement> intermediateList = new ArrayList<>(items);
+        Collections.reverse(intermediateList);
+        mAchievementList.addAll(intermediateList);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        achievementRecycler.setLayoutManager(layoutManager);
-
-        AchievementViewAdapter achievementAdapter = new AchievementViewAdapter(items, this);
-
-        DividerItemDecoration dividerItemDecoration =
-                new DividerItemDecoration(achievementRecycler.getContext(),
-                        LinearLayoutManager.VERTICAL);
-        achievementRecycler.addItemDecoration(dividerItemDecoration);
-
-        achievementRecycler.setAdapter(achievementAdapter);
+        mAchievementAdapter.notifyDataSetChanged();
     }
 }
