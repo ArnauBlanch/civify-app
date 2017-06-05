2f00fabfe27ba4c0db351b339bf5f5c46811c5bf
Sergio Sanchis <sergio.sanchis@est.fib.upc.edu>
2017-06-05 17:34:15 +0200

    [TASK-230] Fragment with API call to test it implemented

--- app/src/main/java/com/civify/activity/fragments/EventsFragment.java
+++ app/src/main/java/com/civify/activity/fragments/EventsFragment.java
@@ -2,12 +2,19 @@ package com.civify.activity.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.civify.R;
 import com.civify.activity.DrawerActivity;
+import com.civify.adapter.event.EventAdapter;
+import com.civify.model.event.Event;
+import com.civify.service.event.ListEventsSimpleCallback;
+import com.civify.utils.AdapterFactory;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +22,8 @@ import com.civify.activity.DrawerActivity;
  * create an instance of this fragment.
  */
 public class EventsFragment extends BasicFragment {
+
+    private static final String TAG_TEST_EVENT = "tag_test_event";
 
     public EventsFragment() {
     }
@@ -46,6 +55,44 @@ public class EventsFragment extends BasicFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //test();
         return inflater.inflate(R.layout.fragment_events, container, false);
+    }
+
+    private void test() {
+        AdapterFactory adapterFactory = AdapterFactory.getInstance();
+        EventAdapter eventAdapter = adapterFactory.getEventAdapter(getContext());
+        eventAdapter.getEvents(new ListEventsSimpleCallback() {
+            @Override
+            public void onSuccess(List<Event> events) {
+                printEvents(events);
+            }
+
+            @Override
+            public void onFailure() {
+                Log.d(TAG_TEST_EVENT, "FAIL");
+            }
+        });
+    }
+
+    private void printEvents(List<Event> events) {
+        for (int i = 0; i < events.size(); i++) {
+            Event event = events.get(i);
+            Log.d(TAG_TEST_EVENT, "Event number " + (i + 1));
+            Log.d(TAG_TEST_EVENT, "Title: " + event.getTitle());
+            Log.d(TAG_TEST_EVENT, "Description: " + event.getDescription());
+            Log.d(TAG_TEST_EVENT, "Number: " + event.getNumber());
+            Log.d(TAG_TEST_EVENT, "Coins: " + event.getCoins());
+            Log.d(TAG_TEST_EVENT, "XP: " + event.getXp());
+            Log.d(TAG_TEST_EVENT, "Created at: " + event.getCreatedAt());
+            Log.d(TAG_TEST_EVENT, "Updated at: " + event.getUpdatedAt());
+            Log.d(TAG_TEST_EVENT, "Kind: " + event.getKind());
+            Log.d(TAG_TEST_EVENT, "Event_token: " + event.getToken());
+            Log.d(TAG_TEST_EVENT, "Start date: " + event.getStartDate());
+            Log.d(TAG_TEST_EVENT, "End date: " + event.getEndDate());
+            Log.d(TAG_TEST_EVENT, "Enabled: " + event.isEnabled());
+            Log.d(TAG_TEST_EVENT, "Picture: " + event.getPicture());
+            Log.d(TAG_TEST_EVENT, "Badge: " + event.getBadge());
+        }
     }
 }
