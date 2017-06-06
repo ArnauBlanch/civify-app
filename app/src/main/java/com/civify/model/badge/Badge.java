38a881c7d841aa74eb1519b49b24a3763efdc129
Alcasser <alcacer.la.1001@gmail.com>
2017-06-06 12:36:21 +0200

    badge model and adapter in factory

--- app/src/main/java/com/civify/model/badge/Badge.java
+++ app/src/main/java/com/civify/model/badge/Badge.java
@@ -1,5 +1,46 @@
 package com.civify.model.badge;
 
-public class Badge {
+import com.civify.utils.ServiceGenerator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
+
+public class Badge implements Serializable {
+
+    @Expose
+    @SerializedName("title")
+    private String mTitle;
+
+    @Expose
+    @SerializedName("content_type")
+    private String mContentType;
+
+    @Expose
+    @SerializedName("large_url")
+    private String mUrl;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getContentType() {
+        return mContentType;
+    }
+
+    public void setContentType(String contentType) {
+        mContentType = contentType;
+    }
+
+    public String getUrl() {
+        return ServiceGenerator.BASE_URL + mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
 }
