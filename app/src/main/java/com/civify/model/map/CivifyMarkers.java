0fadd32e8935803ec83ff410fcd5fbc30aee8f88
Carleslc <lazaro.costa.carles@gmail.com>
2017-06-06 22:13:04 +0200

    Marker Cluster, default markers

--- app/src/main/java/com/civify/model/map/CivifyMarkers.java
+++ app/src/main/java/com/civify/model/map/CivifyMarkers.java
@@ -4,9 +4,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.maps.android.clustering.ClusterManager;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -18,18 +20,15 @@ public class CivifyMarkers implements OnMarkerClickListener {
     public static final String TAG = CivifyMarkers.class.getSimpleName();
 
     private HashMap<String, IssueMarker> mMarkers = new HashMap<>();
-    private CivifyMap mMap;
+    private ClusterManager<IssueMarker> mClusterManager;
 
     CivifyMarkers(@NonNull CivifyMap map) {
         attachToMap(map);
     }
 
     public final void attachToMap(@NonNull CivifyMap map) {
-        mMap = map;
-        map.getGoogleMap().setOnMarkerClickListener(this);
-        if (!mMarkers.isEmpty()) {
-            for (IssueMarker marker : getAll()) marker.attachToMap(map.getGoogleMap());
-        }
+        setUpClusterer(map);
+        addAll(getAll());
     }
 
     @Nullable
@@ -56,7 +55,10 @@ public class CivifyMarkers implements OnMarkerClickListener {
     public void add(@NonNull IssueMarker issueMarker) {
         String tag = issueMarker.getTag();
         mMarkers.put(idify(tag), issueMarker);
-        issueMarker.attachToMap();
+
+        //issueMarker.attachToMap();
+        mClusterManager.addItem(issueMarker);
+        mClusterManager.cluster();
         Log.v(TAG, "Added marker " + tag + '(' + issueMarker.getIssue().getTitle() + ')');
     }
 
@@ -66,7 +68,11 @@ public class CivifyMarkers implements OnMarkerClickListener {
 
     public void remove(@NonNull String tag) {
         IssueMarker issueMarker = mMarkers.remove(idify(tag));
-        if (issueMarker != null && issueMarker.isPresent()) issueMarker.remove();
+        if (issueMarker != null && issueMarker.isPresent()) {
+            mClusterManager.removeItem(issueMarker);
+            mClusterManager.cluster();
+            //issueMarker.remove();
+        }
     }
 
     public void clear() {
@@ -85,6 +91,7 @@ public class CivifyMarkers implements OnMarkerClickListener {
 
     @Override
     public boolean onMarkerClick(final Marker marker) {
+        mClusterManager.onMarkerClick(marker);
         Object markerTag = marker.getTag();
         if (markerTag != null) {
             String tag = markerTag.toString();
@@ -105,6 +112,13 @@ public class CivifyMarkers implements OnMarkerClickListener {
     @NonNull
     private static String idify(@NonNull String possibleKey) {
         return possibleKey.toLowerCase();
+    }
+
+    private void setUpClusterer(@NonNull CivifyMap map) {
+        GoogleMap googleMap = map.getGoogleMap();
+        mClusterManager = new ClusterManager<>(map.getContext(), googleMap);
+        googleMap.setOnMarkerClickListener(mClusterManager);
+        googleMap.setOnCameraIdleListener(mClusterManager);
     }
 
     @Override
