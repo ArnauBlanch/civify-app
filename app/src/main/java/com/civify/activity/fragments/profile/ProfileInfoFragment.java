9d6c017e3e5b20a0ab6c3507955e529d5fa05ce6
Carleslc <lazaro.costa.carles@gmail.com>
2017-06-07 19:53:41 +0200

    Checkstyle fix (added TODOs) and minor CivifyMapTest build crash fix

--- app/src/main/java/com/civify/activity/fragments/profile/ProfileInfoFragment.java
+++ app/src/main/java/com/civify/activity/fragments/profile/ProfileInfoFragment.java
@@ -1,7 +1,6 @@
 package com.civify.activity.fragments.profile;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
