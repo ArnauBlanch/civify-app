8a97ef21f7262e652610bb6d8a7196d624a5485b
Alcasser <alcacer.la.1001@gmail.com>
2017-06-07 02:24:20 +0200

    forgot password activity finished no test

--- app/src/main/java/com/civify/activity/forgotpassword/ForgotActivity.java
+++ app/src/main/java/com/civify/activity/forgotpassword/ForgotActivity.java
@@ -1,8 +1,16 @@
 package com.civify.activity.forgotpassword;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,20 +27,70 @@ public class ForgotActivity extends BaseActivity {
     private EditText mEmailEditText;
     private TextView mMessageView;
     private ImageView mIcon;
+    private Boolean mSubmitClickable;
+    private Drawable mOkDrawable;
+    private DialogInterface.OnClickListener mDialogOnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgotten_password_layout);
-        init();
-        setTextWatchers();
-    }
-
-    private void init() {
+        mSubmitClickable = false;
         mUserAdapter = AdapterFactory.getInstance().getUserAdapter();
         mEmailEditText = (EditText) findViewById(R.id.email_input);
         mMessageView = (TextView) findViewById(R.id.email_exists_text);
         mIcon = (ImageView) findViewById(R.id.email_exists_icon);
+        mOkDrawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_checked);
+        init();
+        setTextWatchers();
+    }
+
+    private void initDialogOnClick() {
+        mDialogOnClick = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog,
+                    int which) {
+                dialog.dismiss();
+                finish();
+            }
+        };
+    }
+
+    private void init() {
+        initDialogOnClick();
+        Button submitButton = (Button) findViewById(R.id.button_submit);
+        submitButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSubmitClickable) {
+                    final ProgressDialog dialog = new ProgressDialog(ForgotActivity.this);
+                    dialog.setMessage(getString(R.string.sending_forgot_mail));
+                    dialog.show();
+                    mUserAdapter.sentResetPasswrdEmail(mEmailEditText.getText().toString(),
+                            new ValidationCallback() {
+                                @Override
+                                public void onValidationResponse(int response) {
+                                    switch (response) {
+                                        case UserAdapter.EMAIL_SENT_CODE:
+                                            dialog.dismiss();
+                                            AlertDialog dialogConf =
+                                                    new AlertDialog.Builder(ForgotActivity.this)
+                                                    .create();
+                                            dialogConf.setMessage(getString(R.string.email_sent));
+                                            dialogConf.setButton(DialogInterface.BUTTON_POSITIVE,
+                                                    getString(R.string.ok), mDialogOnClick);
+                                            dialogConf.show();
+                                            break;
+                                        default:
+                                            dialog.setMessage(getString(R.string.email_not_sent));
+                                    }
+                                }
+                            });
+                } else {
+                    setIconAndMessage(R.drawable.ic_cancel, R.string.enter_email, R.color.red);
+                }
+            }
+        });
     }
 
     private void setTextWatchers() {
@@ -52,21 +110,20 @@ public class ForgotActivity extends BaseActivity {
                     public void onValidationResponse(int response) {
                         switch (response) {
                             case UserAdapter.VALID_UNUSED:
+                                mSubmitClickable = false;
                                 setIconAndMessage(
-                                        R.drawable.ic_cancel,
-                                        R.string.email_not_exists,
+                                        R.drawable.ic_cancel, R.string.email_not_exists,
                                         R.color.red);
                                 break;
                             case UserAdapter.INVALID:
+                                mSubmitClickable = false;
                                 setIconAndMessage(
-                                        R.drawable.ic_cancel,
-                                        R.string.invalid_email,
-                                        R.color.red);
+                                        R.drawable.ic_cancel, R.string.invalid_email, R.color.red);
                                 break;
                             default:
+                                mSubmitClickable = true;
                                 setIconAndMessage(
-                                        R.drawable.ic_checked,
-                                        R.string.email_exists_correct,
+                                        R.drawable.ic_checked, R.string.email_exists_correct,
                                         R.color.green);
                         }
                     }
