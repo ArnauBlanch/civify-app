37a262e337c1e9d7d6e93e2e7b68e89fac97eea9
Carleslc <lazaro.costa.carles@gmail.com>
2017-06-09 02:48:44 +0200

    Fixed Mocked GPS Bugs

--- app/src/main/java/com/civify/activity/fragments/profile/ProfileFragment.java
+++ app/src/main/java/com/civify/activity/fragments/profile/ProfileFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
